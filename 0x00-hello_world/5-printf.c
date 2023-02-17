f1f47ef4428f63efd2dbfdcdaa949cbf3bc81864
binaffanu <binaffanu@gmail.com>
2023-02-17 03:09:05 -0800

    pritnf result

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 	
 	/**
 	 * main: entry point
@@ -8,6 +8,6 @@
 
 	int main(void)
 		{
-			printf("with proper grammar, but the outcome is a piece of art");
+			printf("with proper grammar, but the outcome is a piece of art,");
 			return (0);
 		}
