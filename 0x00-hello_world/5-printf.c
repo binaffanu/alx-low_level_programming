3d8ab9e0a1237f2627d11a8d8a1808fdbd6326c0
binaffanu <binaffanu@gmail.com>
2023-02-17 03:15:25 -0800

    pritnf result

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 	
-	/**
-	 * main: entry point
-	 * Description: prit out string using pritf
-	 * Return: will return 0 (success)
-	 */
+/**
+ * main: entry point
+ * Description: prit out string using pritf
+ * Return: will return 0 (success)
+*/
 
 	int main(void)
-		{
-			printf("with proper grammar, but the outcome is a piece of art,");
-			return (0);
-		}
+	{
+		printf("\"with proper grammar, but the outcome is a piece of art,");
+		return (0);
+	}
