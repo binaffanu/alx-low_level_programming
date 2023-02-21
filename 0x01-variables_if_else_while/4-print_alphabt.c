d6314ecd9473da19725da22f44a6d25ad2a41b9a
binaffanu <binaffanu@gmail.com>
2023-02-20 18:46:18 -0800

    Alphabe ex-q,e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print the letters of the alphabet
+ *
+ * Description: print the letters of the alphabet in lower then in uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        char a = 'a';
+	while (a <= 'z')
+	{
+		if (a != 'q' && 'e')
+		{
+			putchar(a);
+		}
+		a++;
+	}
+	putchar('\n');
+return (0);
+}
