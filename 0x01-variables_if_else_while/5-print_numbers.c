bf96bb6a5691e04cb6aada6e9c06d9b111932417
binaffanu <binaffanu@gmail.com>
2023-02-20 19:07:08 -0800

    1-10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print the letters of the alphabet
+ *
+ * Description: print the letters of the alphabet in lower then in uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int y = 0;
+
+	while (y < 10)
+	{
+		printf("$d");
+		y++;
+	}
+        putchar('\n');
+return (0);
+}
