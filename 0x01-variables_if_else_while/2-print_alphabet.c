a6ec0a25a949573f7f130f92fca49d5f237157c6
binaffanu <binaffanu@gmail.com>
2023-02-20 18:08:16 -0800

    Print using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description
+ * Return: always 0 (success)
+*/
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+
+	return 0;
+	
+}
