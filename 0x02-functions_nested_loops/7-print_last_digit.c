71200e522d0e52b3b6c88c6717496f778564a368
binaffanu <binaffanu@gmail.com>
2023-02-23 02:54:00 -0800

    last digit print

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * print_last_digit - prints the last digit of a number.
+ *
+ * @n: number input in long format
+ *
+ * Return: Returns the value of the last digit
+ */
+int print_last_digit(int n)
+{
+	n = (n % 24);
+	if (n < 0)
+	{
+		n = (-1 * n);
+	}
+	_putchar(n + '0');
+	return (n);
+}
