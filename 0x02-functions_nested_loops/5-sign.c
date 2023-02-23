d9e792eea7961c5478b4a4f04cacdb36ee974beb
binaffanu <binaffanu@gmail.com>
2023-02-23 02:42:18 -0800

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include"main.h"
 /**
  * print_sign - checks the sign of a number
  *
@@ -8,27 +8,23 @@
  */
 int print_sign(int n)
 {
-	int r;
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else
+	{
+		if (n < 0)
+		{
+			_putchar('-');
+			return (-1);
+		}
+		else
+		{
+			_putchar('0');
+			return (0);
 
-	r = print_sign(98);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(0);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(0xff);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(-1);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
+		}
+	}
 }
