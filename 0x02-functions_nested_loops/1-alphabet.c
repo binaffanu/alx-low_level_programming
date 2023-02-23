7227be6a8826fac925c6307ded4ac5fd9e64380e
binaffanu <binaffanu@gmail.com>
2023-02-23 00:41:01 -0800

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,18 @@
-#include "main.h"
+#include"main.h"
 
 /**
- * main - check the code
+ * print_alphabet - prints from a to z
  *
- * Return: Always 0.
- */void print_alphabet(void)
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
 {
 	char a = 'a';
 
+	while (a <= 'z')
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
 }
