7926c6cc502a20e87f9f896fc38681c94ce3b7b1
binaffanu <binaffanu@gmail.com>
2023-02-23 15:35:47 -0800

    tested

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include <main.h>
+/**
+ * main - main content goes here!
+ *Description: Describe how to generate random numbers
+ * Return: always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+	i = 0;
+	positive_or_negative();
+	return (0);
+}
