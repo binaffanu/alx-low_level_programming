8f6ff426f89e77e43c079da5e7273d58a5a12c3c
binaffanu <binaffanu@gmail.com>
2023-02-25 03:18:35 -0800

    Debugged

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,22 +7,24 @@
  *
  * Return: output
  */
+
 void print_to_98(int n)
 {
-	if (n <= 98)
-	{
-		while (n < 98)
-		{
-			printf("%d ", n--);
-		}
-		printf("%d ", n);
-	}
-	else
+	if (n >= 98)
 	{
 		while (n > 98)
 		{
-			printf("%d ", n++);
+			printf("%d, ", n--);
 		}
-		printf("%d ", n);
+		printf("%d\n", n);
+	}
+
+	else
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n++);
+		}
+		printf("%d\n", n);
 	}
 }
