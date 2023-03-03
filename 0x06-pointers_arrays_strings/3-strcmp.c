164de0a25d1e9bacb8379972fe7d501b78fb253f
binaffanu <binaffanu@gmail.com>
2023-03-02 16:56:49 -0800

    compared

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,7 +13,14 @@ int _strcmp(char *s1, char *s2)
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
-		i++;
+		{
+			s1++;
+			s2++;
+		}else
+		{
+			return s1 - s2;
+		}
 	}
+
 	return (0);
 }
