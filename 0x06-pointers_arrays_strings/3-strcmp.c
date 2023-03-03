d8d87bb5a73fa8b9ec0ae3d0b3b701f5c42fa63b
binaffanu <binaffanu@gmail.com>
2023-03-02 17:26:28 -0800

    magic

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,19 +8,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
 	{
-		if (s1[i] != s2[i])
-		{
-			s1++;
-			s2++;
-		}else
-		{
-			return s1 - s2;
-		}
+		s1++;
+		s2++;
 	}
+
+	if (*s1 != *s2)
+		return (*s1 - *s2);
 
 	return (0);
 }
