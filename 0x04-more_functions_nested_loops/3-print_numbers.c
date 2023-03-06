e0c70604a27f6e94d7f7df2ab5516d3b434f6172
binaffanu <binaffanu@gmail.com>
2023-03-06 02:25:59 +0100

    print 0 - 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+ *main - startbof the program.
+ * description: print from 0 - 9.
+ * Return: return 0 on success
+ */
+
+ void print_numbers(void)
+{
+	in i;
+	for (i =0; i <= 9; i++)
+	_putchar(i);
+	return (0);
+}
