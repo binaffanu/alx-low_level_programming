10c1baabd634721e9e3f895e9caeefac7c874b07
binaffanu <binaffanu@gmail.com>
2023-03-06 17:37:48 -0800

    updated

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset() function fills the first n bytes of the memory area
+ * @s: Memory area that points to s
+ * @b: constant bytes
+ * @n number of bytes
+ * Return: on success retun s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i[ = b;
+	}
+	return s;
+}
