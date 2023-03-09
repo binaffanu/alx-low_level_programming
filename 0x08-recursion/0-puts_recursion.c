0315f42f9d49c7315cb02daf282893539acfd684
binaffanu <binaffanu@gmail.com>
2023-03-09 11:35:25 +0100

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include"main.h"
+
+/**
+ *_puts - puts the string
+ * _recursion - function to print string
+ * Return: on success return s
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+	return (s);
+}
---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include "unistd.h"
+
+/**
+ * _putchar - print the word putchar in C language
+ * @c: the charactere for printing
+ *
+ * Return:whens success return 1
+ * On error, return -1 
+*/
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
