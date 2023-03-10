c4e75d5e0df5b7ffb499fffe2f5416c3819bb0a6
binaffanu <binaffanu@gmail.com>
2023-03-10 14:46:54 -0800

    Static Libraries

---
+++ 0x09-static_libraries/2-mul.c
@@ -0,0 +1,12 @@
+#include"main.h"
+
+/**
+ * mul - function ti multiply two integers
+ * @a : first int to be multiplied
+ * @b : second int to be multiplied
+ * Return: return 0 on success
+ */
+int mul(int a, int b)
+{
+        return (a * b);
+}
