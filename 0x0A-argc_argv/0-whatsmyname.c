6c77dece179342e9b838a04bb8d2ddf6485fb017
binaffanu <binaffanu@gmail.com>
2023-03-12 12:52:51 -0700

    updated

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
-
+/**
+ * main - print its name followed by new line
+ * @argc: size of the program
+ * @argv: string pointer array to the program
+ * Return: always return 0 on success
+ */
 int main(int argc, char *argv[])
 {
+
 	int i;
-	
+
 	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
