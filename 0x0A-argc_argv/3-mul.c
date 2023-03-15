f28145e9f451509e7f92e93fb27e42303c1aeb24
binaffanu <binaffanu@gmail.com>
2023-03-14 23:06:42 -0700

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 /**
  * main - print its name followed by new line
  * @argc: size of the program
@@ -8,16 +10,16 @@
 int main(int argc, char *argv[])
 {
 
-        int i, sum = 0;
-
+	int i, prod = 0;
+	
 	if (argc > 1)
 	{
 		for (i = 1; i < argc; i++)
 		{	
-        		printf("argv[%d] = %s\n", i, argv[i]);
-			sum += atoi(argv[i]);
+			printf("argv[%d] = %s\n", i, argv[i]);
+			prod *= atoi(argv[i]);
         	}
-		printf("total = %d", sum);
+		printf("total = %d\n", prod);
 	}
-        return (0);
+	return (1);
 }
