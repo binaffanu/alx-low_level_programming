c5a4a9e7151fc539cac996728c6ac5f5692e3622
binaffanu <binaffanu@gmail.com>
2023-03-15 01:45:32 -0700

    Mpltiply

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,11 +1,12 @@
 #include<stdlib.h>
 #include<stdio.h>
- /**
-  *main - prints the multiplication of two integers
-  * @argc: argument count
-  * @argv: argument vector
-  * Return: 0 if true, 1 if false
-*/
+/**
+  * main - program that multiplies two numbers.
+  * @argc: argument sizw
+  * @argv: argument string pointer
+  *
+  * Return: Always zero
+  */
 int main(int argc, char *argv[])
 {
 	int a, b;
