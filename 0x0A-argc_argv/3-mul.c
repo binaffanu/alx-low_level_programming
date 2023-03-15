c8d816fb42c4abfe4967c41f54b137b9c8fa8b46
binaffanu <binaffanu@gmail.com>
2023-03-15 01:34:00 -0700

    Multiply

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,24 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-/**
- * main - print its name followed by new line
- * @argc: size of the program
- * @argv: string pointer array to the program
- * Return: always return 0 on success
- */
+#include<stdlib.h>
+#include<stdio.h>
+ /**
+  *main - prints the multiplication of two integers
+  * @argc: argument count
+  * @argv: argument vector
+  * Return: 0 if true, 1 if false
+*/
 int main(int argc, char *argv[])
 {
+	int a, b;
 
-	int i, x, prod = 0;
-	
 	if (argc == 3)
 	{
-		x = atoi(argv[1]);
-		i = atoi(argv[2]);
-		prod = x * i;
-
-		printf("%d\n", prod);
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		printf("%d\n", a * b);
+		return (0);
 	}
-	return (0);
+	printf("Error\n");
+	return (1);
 }
